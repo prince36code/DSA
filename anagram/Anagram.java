cb659f043dafeed2e64f545cfc131f31df103bae
princetichatonga25@gmail.com <princetichatonga25@gmail.com>
2026-03-03 22:47:01 +0530

    I implement how to check if a two strings are anagrams or not

---
+++ anagram/Anagram.java
@@ -0,0 +1,35 @@
+package anagram;
+import java.util.Scanner;
+public class Anagram {
+   static boolean isAnagram(String str1, String str2){
+       
+         if(str1.length() != str2.length())return false;
+         int[] count = new int[26];
+
+         for(char c:str1.toCharArray()){
+            count[c -'a']++;
+         }
+
+         for(char c: str2.toCharArray()){
+            count[c-'a']--;
+         }
+
+         for(int num: count){
+            if(num != 0)return false;
+         }
+         return true;
+
+    }
+     public static void main(String[] agrs){
+        Scanner input = new Scanner(System.in);
+        String str1 = input.next();
+        String str2 = input.next();
+
+        if(isAnagram(str1,str2)){
+            System.out.println("The two strings are anagram");
+        }else{
+            System.out.println("The two strings are not anagram");
+        }
+
+     }
+}
